ed004b670ea338763ff17de6976b55c8eaf73b00
Vlad <vladan.stamatov@gmail.com>
2023-02-12 21:09:06 +0100

    delete to deactivate road
    replaced delete with deactivate and replace road

--- Assets/Scripts/RoadManager.cs
+++ Assets/Scripts/RoadManager.cs
@@ -12,6 +12,7 @@ public class RoadManager : MonoBehaviour
     private int lastPrefabIndex = 0;
 
     private List<GameObject> activeRoads = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,20 +23,21 @@ public class RoadManager : MonoBehaviour
                 SpawnRoad(0);
             else
                 SpawnRoad();
-
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerTrasform.position.z - safeZone > (spawnZ - numRoadsOnScreen * roadLength))
+        if (playerTrasform.position.z - safeZone > (spawnZ - numRoadsOnScreen * roadLength))
         {
             SpawnRoad();
-            DeleteRoad();
+            DisableRoad();
         }
     }
-    private void SpawnRoad(int prefabIndex = -1) {
+
+    private void SpawnRoad(int prefabIndex = -1)
+    {
         GameObject go;
         if (prefabIndex == -1)
             go = Instantiate(roadPrefabs[RandomPrefabIndex()]) as GameObject;
@@ -45,12 +47,14 @@ public class RoadManager : MonoBehaviour
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += roadLength;
         activeRoads.Add(go);
-    
     }
-    private void DeleteRoad() {
-        Destroy(activeRoads[0]);
+
+    private void DisableRoad()
+    {
+        activeRoads[0].SetActive(false);
         activeRoads.RemoveAt(0);
     }
+
     private int RandomPrefabIndex()
     {
         if (roadPrefabs.Length <= 1)
@@ -62,6 +66,78 @@ public class RoadManager : MonoBehaviour
         }
         lastPrefabIndex = randomIndex;
         return randomIndex;
-    
     }
 }
+
+
+
+
+
+
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class RoadManager : MonoBehaviour
+//{
+//    public GameObject[] roadPrefabs;
+//    private Transform playerTrasform;
+//    private float spawnZ = 0f;
+//    private float roadLength = 5.2f;
+//    private int numRoadsOnScreen = 8;
+//    private float safeZone = 15f;
+//    private int lastPrefabIndex = 0;
+
+//    private List<GameObject> activeRoads = new List<GameObject>();
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        playerTrasform = GameObject.FindGameObjectWithTag("Player").transform;
+//        for (int i = 0; i < numRoadsOnScreen; i++)
+//        {
+//            if (i < 2)
+//                SpawnRoad(0);
+//            else
+//                SpawnRoad();
+
+//        }
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if(playerTrasform.position.z - safeZone > (spawnZ - numRoadsOnScreen * roadLength))
+//        {
+//            SpawnRoad();
+//            DeleteRoad();
+//        }
+//    }
+//    private void SpawnRoad(int prefabIndex = -1) {
+//        GameObject go;
+//        if (prefabIndex == -1)
+//            go = Instantiate(roadPrefabs[RandomPrefabIndex()]) as GameObject;
+//        else
+//            go = Instantiate(roadPrefabs[prefabIndex]) as GameObject;
+//        go.transform.SetParent(transform);
+//        go.transform.position = Vector3.forward * spawnZ;
+//        spawnZ += roadLength;
+//        activeRoads.Add(go);
+
+//    }
+//    private void DeleteRoad() {
+//        Destroy(activeRoads[0]);
+//        activeRoads.RemoveAt(0);
+//    }
+//    private int RandomPrefabIndex()
+//    {
+//        if (roadPrefabs.Length <= 1)
+//            return 0;
+//        int randomIndex = lastPrefabIndex;
+//        while (randomIndex == lastPrefabIndex)
+//        {
+//            randomIndex = Random.Range(0, roadPrefabs.Length);
+//        }
+//        lastPrefabIndex = randomIndex;
+//        return randomIndex;
+
+//    }
+//}
